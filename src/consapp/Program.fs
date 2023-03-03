2a879561d6148e8cad3d360cc13b66490a009485
DorukUlucayHNC <doruk.ulucay@hackandcraft.com>
2023-03-03 08:55:53 +0000

    add the app which should be left out of coverage. regenerate coverage file.

---
+++ src/consapp/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+
+let add a b =
+    Mathematics.add a b
+
+
+let a = 3
+let b = 5
+let t = add 3 5
+
+Console.WriteLine($"{a} + {b} = {t}")
+
+//this file or project should not be covered by any tests.
